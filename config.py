b6daa2985d80feb40c6d1a8767e3908f1f43be99
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-06-03 18:40:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5350929381").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5569528945").split()))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
 START_IMG = getenv("START_IMG")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/Devbotz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/apnihavelixd")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/dragondpworld")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5350929381").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5569528945").split()))
